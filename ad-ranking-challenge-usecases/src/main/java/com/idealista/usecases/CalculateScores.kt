b10724fecd65b5fa281ebe4719e8b81e656fc117
David Romero <davromalc@gmail.com>
2020-10-12 13:22:20 +0200

    Added first ad rule. If the ad has not pictures. the score will be -10

--- ad-ranking-challenge-usecases/src/main/java/com/idealista/usecases/CalculateScores.kt
+++ ad-ranking-challenge-usecases/src/main/java/com/idealista/usecases/CalculateScores.kt
@@ -1,12 +1,24 @@
 package com.idealista.usecases
 
+import com.idealista.domain.Ad
+import com.idealista.domain.AdRepository
 import com.idealista.usecases.score.params.CalculateScoresParams
 import com.idealista.usecases.shared.Either
 import com.idealista.usecases.shared.UseCase
 import com.idealista.usecases.shared.Validation
 
-class CalculateScores : UseCase<CalculateScoresParams, Nothing> {
-    override fun execute(params: CalculateScoresParams): Either<Validation, Nothing> {
-        TODO("Not yet implemented")
+class CalculateScores(private val adRepository: AdRepository) : UseCase<CalculateScoresParams, Any> {
+    override fun execute(params: CalculateScoresParams): Either<Validation, Any> {
+        val list = adRepository.findAll()
+                .map(this::calculateScore)
+        adRepository.saveAll(list)
+        return Either.Right(Any())
+    }
+
+    private fun calculateScore(it: Ad): Ad {
+        if (it.pictures.isEmpty()) {
+            return it.copy(score = -10)
+        }
+        return it
     }
 }
