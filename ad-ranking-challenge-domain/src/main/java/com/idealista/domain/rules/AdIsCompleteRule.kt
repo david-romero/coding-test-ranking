f19087fb7c4e1963a48f7d0482748c69c53ed2b8
David Romero <davromalc@gmail.com>
2020-10-13 18:12:29 +0200

    Added ad rule. If the ad is complete, not blank description, not empty pictures, house size greater than zero, 40 points are sum to the ad score

--- ad-ranking-challenge-domain/src/main/java/com/idealista/domain/rules/AdIsCompleteRule.kt
+++ ad-ranking-challenge-domain/src/main/java/com/idealista/domain/rules/AdIsCompleteRule.kt
@@ -2,14 +2,48 @@ package com.idealista.domain.rules
 
 import com.idealista.domain.Ad
 import com.idealista.domain.Typology
+import com.idealista.domain.rules.AdIsCompleteRule.TypologyCompleteAdScore.Companion.valueOf
 
 class AdIsCompleteRule : ScoreRule {
     override fun apply(ad: Ad): Int {
-        if (ad.typology == Typology.CHALET) {
-            if (!ad.description.isBlank() && ad.pictures.isNotEmpty() && ad.houseSize > 0 && ad.gardenSize ?: 0 > 0) {
-                return ad.score + 40
-            }
+        if (adIsComplete(ad)) {
+            return ad.score + 40
         }
         return ad.score
+    }
+
+    private fun adIsComplete(ad: Ad) = valueOf(ad.typology).getPredicate().invoke(ad)
+
+    private enum class TypologyCompleteAdScore {
+
+        CHALET {
+            override fun getPredicate(): Ad.() -> Boolean {
+                return {
+                    !description.isBlank() &&
+                            pictures.isNotEmpty() &&
+                            houseSize > 0 &&
+                            gardenSize ?: 0 > 0
+                }
+            }
+        },
+        FLAT {
+            override fun getPredicate(): Ad.() -> Boolean {
+                return {
+                    !description.isBlank() &&
+                            pictures.isNotEmpty() &&
+                            houseSize > 0
+                }
+            }
+        };
+
+        abstract fun getPredicate(): Ad.() -> Boolean
+
+        companion object {
+            fun valueOf(typology: Typology) = when (typology) {
+                Typology.FLAT -> FLAT
+                else -> CHALET
+            }
+        }
+
     }
 }
