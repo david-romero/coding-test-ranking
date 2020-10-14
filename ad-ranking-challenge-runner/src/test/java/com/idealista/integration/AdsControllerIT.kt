38a45628afa8b4fd0d06a6e53ec0de0396948f73
David Romero <davromalc@gmail.com>
2020-10-14 16:20:06 +0200

    Implemented failing integration test for the controller `AdsController` and the operation `Show Ads`
    Why: We want to verify that the integration with the REST endpoint is working well

--- ad-ranking-challenge-runner/src/test/java/com/idealista/integration/AdsControllerIT.kt
+++ ad-ranking-challenge-runner/src/test/java/com/idealista/integration/AdsControllerIT.kt
@@ -1,6 +1,12 @@
 package com.idealista.integration
 
+import com.idealista.domain.Ad
+import com.idealista.domain.AdIdentifier
+import com.idealista.domain.PictureIdentifier
+import com.idealista.domain.Typology
+import com.idealista.domain.rules.Ads
 import com.idealista.infrastructure.api.AdsController
+import com.idealista.usecases.ad.params.ShowAdsParams
 import com.idealista.usecases.score.params.CalculateScoresParams
 import com.idealista.usecases.shared.Either
 import com.idealista.usecases.shared.UseCase
@@ -13,6 +19,8 @@ import org.springframework.test.web.reactive.server.WebTestClient
 internal class AdsControllerIT {
 
     private val calculateScores: UseCase<CalculateScoresParams, Any> = mock()
+
+    private val showAds: UseCase<ShowAdsParams, Ads> = mock()
 
     private val client = WebTestClient
             .bindToController(AdsController(calculateScores))
@@ -32,5 +40,68 @@ internal class AdsControllerIT {
         response.expectStatus().isNoContent
     }
 
+    @Test
+    fun `given a show ads request when the HTTP GET Request is received then a 200 ok is received`() {
+        // given
+        given(showAds.execute(ShowAdsParams())).willReturn(Either.Right(Ads(listOf(
+                Ad(IntBasedAdIdentifier(1), Typology.FLAT, "Piso muy bonito", listOf(IntBasedPictureIdentifier(1)), 300),
+                Ad(IntBasedAdIdentifier(2), Typology.CHALET, "Chalet muy bonito", listOf(IntBasedPictureIdentifier(3)), 500, 100)
+        ))))
+
+        // when
+        val response = client.get()
+                .uri("/api/1/ad")
+                .exchange()
+
+        // then
+        response.expectStatus().isOk
+    }
+
+    @Test
+    fun `given a show ads request when the HTTP GET Request is received then the ads are received`() {
+        // given
+        given(showAds.execute(ShowAdsParams())).willReturn(Either.Right(Ads(listOf(
+                Ad(IntBasedAdIdentifier(1), Typology.FLAT, "Piso muy bonito", listOf(IntBasedPictureIdentifier(1)), 300),
+                Ad(IntBasedAdIdentifier(2), Typology.CHALET, "Chalet muy bonito", listOf(IntBasedPictureIdentifier(3)), 500, 100)
+        ))))
+
+        // when
+        val response = client.get()
+                .uri("/api/1/ad")
+                .exchange()
+
+        // then
+        response.expectBody()
+                .jsonPath("$[]").isArray
+                .jsonPath("$[].size").isEqualTo(2)
+                .jsonPath("$[0].id").isEqualTo(1)
+                .jsonPath("$[0].typology").isEqualTo("FLAT")
+                .jsonPath("$[0].description").isEqualTo("Piso muy bonito")
+                .jsonPath("$[0].pictureUrls").isArray
+                .jsonPath("$[0].pictureUrls[].size").isEqualTo(1)
+                .jsonPath("$[0].pictureUrls[0]").isEqualTo("1")
+                .jsonPath("$[0].houseSize").isEqualTo(300)
+                .jsonPath("$[1].id").isEqualTo(2)
+                .jsonPath("$[1].typology").isEqualTo("CHALET")
+                .jsonPath("$[1].description").isEqualTo("Chalet muy bonito")
+                .jsonPath("$[1].pictureUrls").isArray
+                .jsonPath("$[1].pictureUrls[].size").isEqualTo(1)
+                .jsonPath("$[1].pictureUrls[0]").isEqualTo("3")
+                .jsonPath("$[1].houseSize").isEqualTo(500)
+                .jsonPath("$[1].gardenSize").isEqualTo(100)
+    }
+
     private inline fun <reified T : Any> mock(): T = Mockito.mock(T::class.java)
+}
+
+inline class IntBasedAdIdentifier(private val value: Int) : AdIdentifier {
+    override fun toString(): String {
+        return "$value"
+    }
+}
+
+inline class IntBasedPictureIdentifier(private val value: Int) : PictureIdentifier {
+    override fun toString(): String {
+        return "$value"
+    }
 }
